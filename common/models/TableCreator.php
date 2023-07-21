773c878f029cb899e5ae125530724221cdf09ac1
Skoryk Dmytro <skorikdeveloper@gmail.com>
2023-07-21 20:02:52 +0200

    added table creator class

---
+++ common/models/TableCreator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace common\models;
+use Yii;
+
+class TableCreator {
+    private $db;
+
+    public function __construct() {
+        $this->db = Yii::$app->db;
+        $this->createTables();
+    }
+
+    private function createTables(): void {
+        $this->userConnections();
+    }
+
+    private function userConnections(): void {
+        $query = "
+                CREATE TABLE IF NOT EXISTS UserConnections (
+                    connection_id INT PRIMARY KEY AUTO_INCREMENT,
+                    user_id_1 INT NOT NULL,
+                    user_id_2 INT NOT NULL,
+                    status VARCHAR(50) NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                )
+            ";
+        $this->db->createCommand($query)->execute();
+    }
+}
