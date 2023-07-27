39b79953560b7a08a159d6592d1e3e6b96a6ae85
Ilya <z-z-z88@list.ru>
2023-07-28 02:30:03 +0600

    fix typo after merging

--- common/models/TableCreator.php
+++ common/models/TableCreator.php
@@ -6,24 +6,30 @@ use backend\models\UsersWithSharedInterests;
 use Yii;
 
 
-class TableCreator {
+class TableCreator
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = Yii::$app->db;
         $this->createTables();
         $this->updateTables();
     }
 
-    private function createTables(): void {
+    private function createTables(): void
+    {
         $this->userConnections();
         $this->usersWithSharedInterests();
     }
-    private function  updateTables(): void {
+
+    private function updateTables(): void
+    {
         $this->userUpdate();
     }
 
-    private function userConnections(): void {
+    private function userConnections(): void
+    {
         $query = "
                 CREATE TABLE IF NOT EXISTS UserConnections (
                     connection_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -37,15 +43,16 @@ class TableCreator {
         $this->db->createCommand($query)->execute();
     }
 
-    private function userUpdate(): void {
+    private function userUpdate(): void
+    {
         $table = $this->db->schema->getTableSchema('User');
         if (!isset($table->columns['telegram_alias'])) {
             $this->db->createCommand()->addColumn('User', 'telegram_alias', 'varchar(33) AFTER telegram')->execute();
         }
-
-
-    private function usersWithSharedInterests(): void {
-        $query = "
+    }
+        private function usersWithSharedInterests(): void
+        {
+            $query = "
                 CREATE TABLE IF NOT EXISTS UsersWithSharedInterests (
                     id INT PRIMARY KEY AUTO_INCREMENT,
                     user_id_1 INT NOT NULL,
@@ -55,7 +62,8 @@ class TableCreator {
                     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
                 );
             ";
-        $this->db->createCommand($query)->execute();
+            $this->db->createCommand($query)->execute();
 
-    }
+        }
+
 }
