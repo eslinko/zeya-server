69123978d5227a1bdf006ec29b3b604857064954
d.buyanov <d.buyanov@tool-kit.tech>
2023-08-22 09:30:42 +0300

    CHANGE: added app envs

--- environments/prod/backend/web/index.php
+++ environments/prod/backend/web/index.php
@@ -7,6 +7,18 @@ require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
 
+// google geocoding api key
+defined('GoogleMapApiKey') or define('GoogleMapApiKey', env('GOOGLE_MAP_API_KEY'));
+// google vision json file name
+defined('GoogleVisionFile') or define('GoogleVisionFile', env('GOOGLE_VISION_FILE'));
+// telegram bot id
+defined('TelegramBotId') or define('TelegramBotId', env('TELEGRAM_BOT_ID'));
+// chat gpt api key
+defined('ChatGPTApiKey') or define('ChatGPTApiKey', env('CHAT_GPT_API_KEY'));
+// sendgrid api key
+defined('SendGridApiKey') or define('SendGridApiKey', env('SENDGRID_API_KEY'));
+
+
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
     require __DIR__ . '/../../common/config/main-local.php',
