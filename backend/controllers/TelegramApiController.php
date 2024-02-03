1311ab573f0f953f4399ceee3880dd4f15506104
Ilya <z-z-z88@list.ru>
2024-02-03 13:43:33 +0600

    lovedo

--- backend/controllers/TelegramApiController.php
+++ backend/controllers/TelegramApiController.php
@@ -55,7 +55,7 @@ class TelegramApiController extends AppController
         ];
     }*/
     public function beforeAction($action) { //enable incoming POST requests
-        $post_actions = ['notifications-delete','notifications-read','notifications-read-all','set-text-content-to-expression', 'set-user-last-message', 'set-description-to-expression','vote-for-love-do'];
+        $post_actions = ['notifications-delete','notifications-read','notifications-read-all','set-text-content-to-expression', 'set-user-last-message', 'set-description-to-expression'];//,'vote-for-love-do'];
         if(in_array($action->id, $post_actions)) {
             $this->enableCsrfValidation = false;
         }
@@ -1872,7 +1872,7 @@ class TelegramApiController extends AppController
     }
     public function actionVoteForLoveDO(){
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        $data = \Yii::$app->request->post();
+        $data = \Yii::$app->request->get();
 
         $res = TelegramApi::validateWebAppRequest($data['initData']);
         if ($res['status'] == false OR isset($data['creative_expression_id']) == false) {
